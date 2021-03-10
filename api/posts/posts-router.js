03fe9c379e1f51d03b27e66d060da23333e642a2
Bansa Laura <weidiwu207@gmail.com>
2021-03-09 23:25:07 -0500

    met mvp again

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -61,9 +61,41 @@ router.put('/:id', (req,res) => {
     .catch(() => {
         res.status(500).json({message:"The post information could not be retrieved"})
     })
-
-
+})
+router.delete('/:id', (req,res) => {
+    Posts.findById(req.params.id)
+    .then((post) => {
+        if(!post)
+        res.status(404).json({message:"The post with the specified ID does not exist"})
+        else
+            Posts.remove(req.params.id)
+            .then(() => {
+                Posts.findById(req.params.id)
+                .then((post) => {
+                    res.status(200).json(post)
+                })
+                  
+            })
+            .catch(() => {
+                res.status(500).json({message: "The post could not be removed" })
+            })
+    })
 })
 
+router.get('/:id/comments', (req,res) => {
+    Posts.findById(req.params.id)
+    .then((post) => {
+        if(!post)
+        res.status(404).json({message:"The post with the specified ID does not exist"})
+        else
+            Posts.findPostComments(req.params.id)
+            .then((post) => {
+                res.status(200).json(post)
+            })
+            .catch(() => {
+                res.status(500).json({message: "The comments information could not be retrieved" })
+            })
+    })
 
+})
 module.exports = router
